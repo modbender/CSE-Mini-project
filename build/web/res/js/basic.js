9101aef55bb1627ba019922084dea7cd08ca7fa5
yashas123 <rameshmamathayashas@gmail.com>
2018-10-22 13:59:29 +0530

    small changes

--- build/web/res/js/basic.js
+++ build/web/res/js/basic.js
@@ -3,10 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+//const changeColor = (elem) => {
+//    var rand,color = "";
+//    do{
+//        rand = Math.floor(Math.random() * 90);
+//        color = color + rand;
+//    }while(color.length<6);
+//    console.log(color);
+//    elem.css('color',"#"+color);
+//    setTimeout(function(){
+//        changeColor(elem);
+//    },500);
+//};
 
 $(document).ready(function () {
-    var kvid = $("#kbc-vid").get(0),sbtn = $("#state-btn"),st = $("#state"), vbtn=$("#volume-btn"),vol = $("#volume");
+    var kvid = $("#kbc-vid").get(0),sbtn = $("#state-btn"),st = $("#state"), vbtn=$("#volume-btn"),vol = $("#volume"),
+        inputs = $("input[type='text'],input[type='password'],input[type='email'],input[type='tel']"),
+        title = $(".panel-title");
+//    changeColor(title);
     kvid.currentTime = 25;
     sbtn.click(function(){
         st.toggleClass("fa-play fa-pause");
@@ -17,8 +31,8 @@ $(document).ready(function () {
         kvid.muted = kvid.muted ? false : true ;
     });
     setTimeout(function () {
-        $("input[type='text'],input[type='password'],input[type='email'],input[type='tel']").attr('readonly', false);
-        $("input[type='text'],input[type='password'],input[type='email'],input[type='tel']").focus();
+        inputs.attr('readonly', false);
+        inputs.focus();
     }, 500);
     if ($(".flash-bg") !== null) {
         flashbg();
--- build/web/user/profile.jsp
+++ build/web/user/profile.jsp
@@ -4,16 +4,32 @@
     Author     : Yashas Hr
 --%>
 
+<%@page import="java.sql.ResultSet"%>
+<%@page import="static dbconn.Connect.st"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
-        <jsp:include page="/layout/head.jsp"/>
+        <jsp:include page="/head"/>
         <title>Profile</title>
     </head>
     <body>
-        <jsp:include page="/layout/header.jsp"/>
-        
-        <jsp:include page="/layout/footer.jsp"/>
+        <jsp:include page="/header"/>
+        <%
+            session = request.getSession(false);
+            if(session.getAttribute("name") != null){
+                dbconn.Connect.main(null);
+                ResultSet rs = st.executeQuery("select * from users where uname='"+session.getAttribute("name")+"';");
+                rs.next();
+                String name = "";
+        %>
+        <script>
+            document.title = "<%=name%> | "+document.title;
+        </script>
+        <%=name%>
+        <% } else { %>
+        <h2>Not logged in</h2>
+        <% } %>
+        <jsp:include page="/footer"/>
     </body>
 </html>
